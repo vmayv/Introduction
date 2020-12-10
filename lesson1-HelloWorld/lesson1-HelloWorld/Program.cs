020d2a27343b49523cc43d8b51ab36056b5f633f
vasiliy.mayorov <vasyamayorov@yandex.ru>
2020-12-10 15:32:22 +0300

    Выполнено домашнее задание

---
+++ lesson1-HelloWorld/lesson1-HelloWorld/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson1_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ваше имя:");
+            string name = Console.ReadLine();
+            Console.WriteLine($"Привет, {name}, сегодня {DateTime.Today.ToShortDateString()}");
+            Console.ReadKey();
+        }
+    }
+}
